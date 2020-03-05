4bf28fa953ced04491524998506fd66a4a075124
JC <blahblahblah@email.com>
2020-03-05 10:08:17 -0600

    Add project files.

---
+++ scratchpad/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace scratchpad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            char firstLetterOfName = name[0];
+            Console.WriteLine(firstLetterOfName);
+            
+            
+            //reverse
+            string reversed = Reverse(name);
+            char lastLetter = reversed[0];
+            Console.WriteLine(lastLetter);
+
+            char star = '*';
+
+            Console.WriteLine(star);
+           
+            Console.ReadKey();
+        }
+
+        private static string Reverse(string name)
+        {
+            char[] charArray = name.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+
+            //found on stack overflow
+            //https://stackoverflow.com/questions/228038/best-way-to-reverse-a-string
+        }
+    }
+}
